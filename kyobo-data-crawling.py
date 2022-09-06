2d5717ef3e8de9339352dee950cc7fb832c69e27
EunHo Bang (방은호) <80435502+eunhobang@users.noreply.github.com>
2022-09-06 09:45:18 +0900

    Update kyobo-data-crawling.py

--- kyobo-data-crawling.py
+++ kyobo-data-crawling.py
@@ -4,9 +4,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-URL = 'http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?mallGb=KOR&linkClass=B&range=1&kind=2&orderClick=DAd'
+# URL = 'http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?mallGb=KOR&linkClass=B&range=1&kind=2&orderClick=DAd'
+URL = 'http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?mallGb=KOR&linkClass=c&range=1&kind=2&orderClick=DAd' #자기계발
 
-driver = webdriver.Chrome('/Users/boyeonjang/git/kyobobook-review/chromedriver')
+# driver = webdriver.Chrome('/Users/boyeonjang/git/kyobobook-review/chromedriver')
+driver = webdriver.Chrome('C:/Users/eunho/Workspace/kyobobook-review/chromedriver.exe')
+
 
 driver.get(url=URL)
 
@@ -23,7 +26,8 @@ for book in range(1, 21): # top 20
 
   # try:
   # 페이지가 다음으로 넘어가면
-  for page in range(1, 300):
+  for n in range(1, 20):
+    sleep(1)
     for idx in range(1, 6): # 리뷰 5개씩
       rating_xpath = driver.find_element(By.XPATH, f'''//*[@id="box_detail_review"]/ul/li[{idx}]/div[1]/dl/dd[3]/span''') # 별점
       text_xpath = driver.find_element(By.XPATH, f'''//*[@id="box_detail_review"]/ul/li[{idx}]/div[1]/dl/dd[5]/div''') # 리뷰 글
@@ -38,16 +42,28 @@ for book in range(1, 21): # top 20
       driver.implicitly_wait(time_to_wait=5)
   # except:
   #   break
-  
-    paging_num_xpath = driver.find_element(By.XPATH, '''//*[@id="box_detail_review"]/div[3]/div''')
-    paging_num = paging_num_xpath.text
+    page_bar = driver.find_elements(By.CSS_SELECTOR,'#box_detail_review > div.list_paging.align_center > div >*')
     
-    print("paging_num 확인==============", paging_num)
-    print("paging_num 타입 확인==============", type(paging_num))
-    if page%10 != 0:
-      paging_num[page%10+1].click()
+    if n%5 != 0:
+        print("++++++++")
+        page_bar[n%10+1].click()
+        sleep(1)
+
+    # 현재 페이지(n)가 5의 배수인 경우 다음페이지 버튼을 클릭
     else:
-      paging_num[11].click()
+        print("=========")
+        page_bar[11].click()
+        sleep(1)
+  
+    # paging_num_xpath = driver.find_element(By.XPATH, '''//*[@id="box_detail_review"]/div[3]/div''')
+    # paging_num = paging_num_xpath.text
+    
+    # print("paging_num 확인==============", paging_num)
+    # print("paging_num 타입 확인==============", type(paging_num))
+    # if page%10 != 0:
+    #   paging_num[page%10+1].click()
+    # else:
+    #   paging_num[11].click()
 
     # 다음페이지 넘기기
     # driver.find_element(By.XPATH, f'''//*[@id="box_detail_review"]/div[3]/div/a[10]''').click()
