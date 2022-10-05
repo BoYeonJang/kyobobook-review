344c17d89e6b5bf534a73c8c0fa4f895092a47ba
BoYeonJang <uko010199@gmail.com>
2022-10-05 20:55:45 +0900

    backend: 코드 수정

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import torch
+from inference import *
 
+model = torch.load('../Data/모델/model.pt')
+
+# Flask 객체 인스턴스 생성
 app = Flask(__name__)
+CORS(app)
 
+# 접속 URL 설정
 @app.route('/')
 def index():
-  '''Index page route'''
+  return f'{model}'
 
-  return '<h1>Application Deployed!</h1>'
+@app.route('/api', methods=['POST'])
+def api():
+  query = request.get_json()
+  print("query: ", query)
+  output_data = inference_fn(
+    query["prompt"],
+    query["min_length"],
+    query["max_length"],
+    query["top_p"],
+    query["top_k"],
+    query["repetition_penalty"],
+    query["no_repeat_ngram_size"],
+    query["temperature"])
+  response = jsonify(output_data)
+  print("response: ", response)
+  print("output_data: ", output_data)
+
+  return response
+
+# @app.route('/api', methods=['GET'])
+# def api():
+  # query = request.json
+  # output_data = inference_fn("이건테스트입니다.ㄴ아너아ㅣㅇ너리ㅏㅁㄴ;ㅓ")
+  # response = jsonify(output_data)
+
+  # return inference_fn("이건테스트입니다.ㄴ아너아ㅣㅇ너리ㅏㅁㄴ;ㅓ")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+  if request.method == 'POST':
+    data = request.get_json()
+    print(data)
+
+    return jsonify({'resultType': 1, 'data': data})
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  # 코드 수정 시 자동 반영
+  app.run(host='0.0.0.0', debug=True)
--- backend/inference.py
+++ backend/inference.py
@@ -1,6 +1,6 @@
 import torch
 from arguments import *
-from transformers import PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerFast, GPT2Config, GPT2LMHeadModel
 
 # 인퍼런스 설정
 args = GenerationDeployArguments(
@@ -16,12 +16,9 @@ tokenizer = PreTrainedTokenizerFast.from_pretrained(
 
 model = torch.load('../Data/모델/model.pt')
 
-from transformers import GPT2Config, GPT2LMHeadModel
-if args.downstream_model_checkpoint_fpath is None:
-    model = GPT2LMHeadModel.from_pretrained(
-        args.pretrained_model_name,
-    )
-# model.eval()
+model = GPT2LMHeadModel.from_pretrained(
+  args.pretrained_model_name,
+)
 
 def inference_fn(
   prompt,
@@ -34,31 +31,38 @@ def inference_fn(
   temperature=1.0
   ):
   try:
+    # generated_sentence = """성공입니다."""
     input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    print("start")
+    print("prompt: ", prompt)
     with torch.no_grad():
       generated_ids = model.generate(
         input_ids,
         do_sample=True,
-        top_p=float(top_p),
-        top_k=int(top_k),
         min_length=int(min_length),
         max_length=int(max_length),
+        top_p=float(top_p),
+        top_k=int(top_k),
         repetition_penalty=float(repetition_penalty),
         no_repeat_ngram_size=int(no_repeat_ngram_size),
         temperature=float(temperature),
       )
     generated_sentence = tokenizer.decode([el.item() for el in generated_ids[0]])
-  except:
-    generated_sentence = """처리 중 오류가 발생했습니다. <br>
-      변수의 입력 범위를 확인하세요. <br><br>
-      min_length: 1 이상의 정수 <br>
-      max_length: 1 이상의 정수 <br>
-      top-p: 0 이상 1 이하의 실수 <br>
-      top-k: 1 이상의 정수 <br>
-      repetition_penalty: 1 이상의 실수 <br>
-      no_repeat_ngram_size: 1 이상의 정수 <br>
-      temperature: 0 이상의 실수
-      """
+    print("generated_sentence:", generated_sentence)
+  except Exception as e:
+    # generated_sentence = """처리 중 오류가 발생했습니다. <br>
+    #   변수의 입력 범위를 확인하세요. <br><br>
+    #   min_length: 1 이상의 정수 <br>
+    #   max_length: 1 이상의 정수 <br>
+    #   top-p: 0 이상 1 이하의 실수 <br>
+    #   top-k: 1 이상의 정수 <br>
+    #   repetition_penalty: 1 이상의 실수 <br>
+    #   no_repeat_ngram_size: 1 이상의 정수 <br>
+    #   temperature: 0 이상의 실수
+    #   """
+    generated_sentence = """실패했습니다."""
+    print("eeeeeeeeeeee", e)
   return {
     'result': generated_sentence,
   }
+  
