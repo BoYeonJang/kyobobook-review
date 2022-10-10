ab9a2bb9111f8049d349e38111a7fb7bddbb1701
BoYeonJang <uko010199@gmail.com>
2022-10-10 11:32:59 +0900

    frontend: css 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <HelloWorld />
+  <div
+    class="font-IBM text-center text-black bg-gradient-to-r from-[#6ADE64] to-[#36777C] w-screen h-full"
+  >
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import "./style.css";
 
 export default {
   name: "App",
@@ -12,14 +17,7 @@ export default {
   },
 };
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
