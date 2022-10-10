c1794cc3de91416e7751c0dfd679fee7a8af6e57
BoYeonJang <uko010199@gmail.com>
2022-10-10 11:33:34 +0900

    frontend: input 창 수정

--- frontend/src/components/SentenceGeneration.vue
+++ frontend/src/components/SentenceGeneration.vue
@@ -1,16 +1,40 @@
-<template>
+<template v-if="generationTrigger">
   <div>
-    <div class="input-group mb-3">
-      <input
-        type="text"
-        class="review_search"
-        aria-describedby="basic-addon3"
-        ref="prompt"
-        id="prompt"
-        @keypress.enter="api_call"
-        name="input"
-        placeholder="프롬프트를 입력하세요"
-      />
+    <div class="text-center pl-96">
+      <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+        >Search</label
+      >
+      <div class="relative mt-10 content-center">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          class="block p-4 pl-10 w-96 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          aria-describedby="basic-addon3"
+          ref="prompt"
+          id="prompt"
+          @keypress.enter="api_call"
+          name="input"
+          placeholder="프롬프트를 입력하세요"
+        />
+      </div>
     </div>
     <div class="alert alert-info" role="alert">
       <b>생성 결과</b>
@@ -45,6 +69,7 @@ export default {
       repetition_penalty: 1.0,
       no_repeat_ngram_size: 0,
       temperature: 1.0,
+      generationTrigger: false,
     };
   },
   methods: {
@@ -68,6 +93,7 @@ export default {
           console.log("body: ", body);
           console.log("this.$refs.generation: ", this.$refs.generation);
           this.$refs.generation.innerText = body["result"];
+          this.generationTrigger = true;
         })
         .catch(err => console.error("실패했습니다.", err));
     },
@@ -102,13 +128,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.review_search {
-  /* margin: 20px; */
-  padding: 15px;
-  border: 1px solid black;
-  border-radius: 5px;
-  width: 40rem;
-  font-size: 15px;
-}
-</style>
+<style scoped></style>
