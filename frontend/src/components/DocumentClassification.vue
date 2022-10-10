2d3cd919e04437c18abf6f15c315573118e0f69e
BoYeonJang <uko010199@gmail.com>
2022-10-10 11:33:16 +0900

    frontend: css 수정

--- frontend/src/components/DocumentClassification.vue
+++ frontend/src/components/DocumentClassification.vue
@@ -1,12 +1,12 @@
-<template>
+<template v-if="trigger">
   <div>
-    <p>
-      <b>오늘은 교양을 쌓고 싶어</b>에 어울리는책의 종류는 <b>교양과학</b>입니다.<br /><b
-        >교양과학</b
-      >
-      분야의 베스트 셀러는 다음과 같습니다.
-    </p>
-    <table class="book_table">
+    <div>
+      <p class="font-bold underline decoration-indigo-500">오늘은 교양을 쌓고 싶어</p>
+      에 어울리는책의 종류는
+      <p class="font-bold underline decoration-orange-500">교양과학</p>
+      입니다.<br />해당 분야의 베스트 셀러는 다음과 같습니다.
+    </div>
+    <table class="table-auto">
       <th>교양과학</th>
       <tr>
         <td>코스모스</td>
@@ -32,24 +32,12 @@
 
 export default {
   name: "DocumentClassification",
-  props: {},
+  props: {
+    trigger: Boolean,
+  },
   data: function () {
     return {};
   },
   methods: {},
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-th,
-td {
-  border-bottom: 1px solid black;
-  border-collapse: collapse;
-}
-.book_table {
-  padding: 20px;
-  margin-left: auto;
-  margin-right: auto;
-}
-</style>
