8135a8dd3f19a597886a3aead0a3a47f53f534c5
BoYeonJang <uko010199@gmail.com>
2022-10-11 14:50:29 +0900

    frontend: 과학 테이블, 인문 테이블 추가

---
+++ frontend/src/components/HumanTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <table v-for="item in items" :key="item.title">
+      <tr>
+        <td rowspan="6"><img v-bind:src="item.image" /></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.title"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.pubDate"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.writer"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.publisher"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.userRating"></span></td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "HumanTable",
+  props: {},
+  data: function () {
+    return {
+      items: [
+        {
+          title: "<strong>사피엔스</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9788934972464.jpg",
+          pubDate: "2015.11.24 출시",
+          writer: "유발 하라리 외",
+          publisher: "김영사",
+          userRating: "9.58",
+        },
+        {
+          title: "<strong>소크라테스 익스프레스</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9791190030922.jpg",
+          pubDate: "2021.04.28 출시",
+          writer: "에릭 와이너 외",
+          publisher: "어크로스",
+          userRating: "9.59",
+        },
+        {
+          title: "<strong>공정하다는 착각</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9791164136452.jpg",
+          pubDate: "2020.12.01 출시",
+          writer: "마이클 샌델 외",
+          publisher: "와이즈베리",
+          userRating: "9.38",
+        },
+        {
+          title: "<strong>질서 너머</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9788901249599.jpg",
+          pubDate: "2021.03.22 출시",
+          writer: "조던 피터슨 외",
+          publisher: "웅진지식하우스",
+          userRating: "9.29",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+table {
+  font-size: 1.25rem;
+  text-align: left;
+  border: 1px solid #ccc;
+}
+.float-left {
+  float: left;
+}
+</style>
---
+++ frontend/src/components/ScienceTable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <table v-for="item in items" :key="item.title">
+      <tr>
+        <td rowspan="6"><img v-bind:src="item.image" /></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.title"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.pubDate"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.writer"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.publisher"></span></td>
+      </tr>
+      <tr>
+        <td><span v-html="item.userRating"></span></td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "ScienceTable",
+  props: {},
+  data: function () {
+    return {
+      items: [
+        {
+          title: "<strong>코스모스</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9788983711892.jpg",
+          pubDate: "2010.01.20 출시",
+          writer: "칼 세이건 외",
+          publisher: "사이언스북스",
+          userRating: "9.66",
+        },
+        {
+          title: "<strong>이기적 유전자(40주년 기념판)</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9788932473901.jpg",
+          pubDate: "2018.10.20 출시",
+          writer: "리처드 도킨스 외",
+          publisher: "을유문화사",
+          userRating: "9.52",
+        },
+        {
+          title: "<strong>엔드 오브 타임</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9791164137411.jpg",
+          pubDate: "2021.02.15 출시",
+          writer: "브라이언 그린 외",
+          publisher: "와이즈베리",
+          userRating: "9.69",
+        },
+        {
+          title: "<strong>다정한 것이 살아남는다</strong>",
+          image: "https://contents.kyobobook.co.kr/sih/fit-in/142x0/pdt/9791197413025.jpg",
+          pubDate: "2021.07.26 출시",
+          writer: "브라이언 헤어 외",
+          publisher: "디플롯",
+          userRating: "9.64",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+table {
+  font-size: 1.25rem;
+  text-align: left;
+  border: 1px solid #ccc;
+}
+.float-left {
+  float: left;
+}
+</style>
