92b7a84bba1e9f131faec2cd9e5482d49aecb776
BoYeonJang <uko010199@gmail.com>
2022-10-13 13:34:09 +0900

    backend: 모델 로드 수정

--- backend/generation_inference.py
+++ backend/generation_inference.py
@@ -5,7 +5,7 @@ from transformers import PreTrainedTokenizerFast, GPT2Config, GPT2LMHeadModel
 # 인퍼런스 설정
 args = GenerationDeployArguments(
     pretrained_model_name="skt/kogpt2-base-v2",
-    downstream_model_dir="../Data/모델/사피엔스/checkpoint-generation",
+    downstream_model_dir="../Data/모델/top3/checkpoint-generation",
 )
 
 # 토크나이저 로드
@@ -15,12 +15,31 @@ tokenizer = PreTrainedTokenizerFast.from_pretrained(
 )
 
 # 모델 로드
-model = torch.load('../Data/모델/사피엔스/Sapiens.pt')
-model.eval()
 
-model = GPT2LMHeadModel.from_pretrained(
+# model = torch.load('../Data/모델/top3/3total_model')
+
+# model = GPT2LMHeadModel.from_pretrained(
+#     args.pretrained_model_name,
+# )
+
+pretrained_model_config = GPT2Config.from_pretrained(
     args.pretrained_model_name,
 )
+
+model = GPT2LMHeadModel(pretrained_model_config)
+
+fine_tuned_model_ckpt = torch.load(
+    args.downstream_model_checkpoint_fpath,
+    map_location=torch.device("cpu"),
+)
+
+
+# 체크포인트 읽기
+model.load_state_dict({k.replace("model.", ""): v for k,
+                      v in fine_tuned_model_ckpt['state_dict'].items()})
+
+# 평가 모드 전환
+model.eval()
 
 # 인퍼런스 설정
 
