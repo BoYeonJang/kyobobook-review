cee8ad40edf2bfa893f6d8ceddf8783683fc694f
BoYeonJang <uko010199@gmail.com>
2022-10-13 16:03:05 +0900

    frontend: logo바 추가

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,57 +1,57 @@
 <template>
-  <div class="font-NotoSans downUp">
+  <div class="font-NotoSans">
     <!-- logo -->
-    <div>
-      <span class="font-bold text-xl">A<span class="signature-color">i</span>Letter</span>
-    </div>
+    <LogoBar />
     <!-- main -->
-    <div class="hello">
-      <div class="mt-13">
-        <span>안녕하세요.</span>
+    <div class="downUp">
+      <div class="hello">
+        <div class="mt-13">
+          <span>안녕하세요.</span>
+        </div>
+        <div class="mt-2">
+          <span>오늘은 어떤 책을 읽고 싶은지 </span>
+          <span class="signature-color font-bold">당신의 생각</span>을 적어주세요.
+        </div>
       </div>
-      <div class="mt-2">
-        <span>오늘은 어떤 책을 읽고 싶은지 </span>
-        <span class="signature-color font-bold">당신의 생각</span>을 적어주세요.
+      <!-- search bar -->
+      <div class="search-bar">
+        <div class="search-svg">
+          <svg
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="search-img"
+          >
+            <path
+              d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            ></path>
+          </svg>
+          <input
+            type="text"
+            class="think-search"
+            ref="prompt"
+            @keypress.enter="api_call"
+            name="input"
+            placeholder="당신의 생각을 적어주세요."
+          />
+        </div>
       </div>
-    </div>
-    <!-- search bar -->
-    <div class="search-bar">
-      <div class="search-svg">
-        <svg
-          focusable="false"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="search-img"
-        >
-          <path
-            d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
-          ></path>
-        </svg>
-        <input
-          type="text"
-          class="think-search"
-          ref="prompt"
-          @keypress.enter="api_call"
-          name="input"
-          placeholder="당신의 생각을 적어주세요."
-        />
-      </div>
-    </div>
-    <!-- table -->
-    <div v-if="ClassificationTrigger" class="text-xl">
-      <span class="decoration-indigo font-bold">{{ this.sentence }}</span
-      >에 어울리는 분야는 <span class="decoration-indigo font-bold">{{ this.prediction }}</span
-      >입니다.
-      <br />
-      <span class="">해당 분야의 베스트 셀러는 다음과 같습니다.</span>
-      <div v-if="this.prediction == '과학'">
-        <ScienceTable />
-      </div>
-      <div v-else-if="this.prediction == '인문'">
-        <HumanTable />
-      </div>
-      <div v-else-if="this.prediction == '소설'">
-        <NovelTable />
+      <!-- table -->
+      <div v-if="ClassificationTrigger" class="text-xl">
+        <span class="decoration-indigo font-bold">{{ this.sentence }}</span
+        >에 어울리는 분야는 <span class="decoration-indigo font-bold">{{ this.prediction }}</span
+        >입니다.
+        <br />
+        <span class="">해당 분야의 베스트 셀러는 다음과 같습니다.</span>
+        <div v-if="this.prediction == '과학'">
+          <ScienceTable />
+        </div>
+        <div v-else-if="this.prediction == '인문'">
+          <HumanTable />
+        </div>
+        <div v-else-if="this.prediction == '소설'">
+          <NovelTable />
+        </div>
       </div>
     </div>
   </div>
@@ -61,6 +61,7 @@
 import ScienceTable from "./ScienceTable.vue";
 import HumanTable from "./HumanTable.vue";
 import NovelTable from "./NovelTable.vue";
+import LogoBar from "./LogoBar.vue";
 import * as API from "../api.js";
 
 export default {
@@ -70,6 +71,7 @@ export default {
     ScienceTable,
     HumanTable,
     NovelTable,
+    LogoBar,
   },
   data: function () {
     return {
@@ -177,7 +179,7 @@ input::placeholder {
   margin-top: 2rem;
 }
 .mt-13 {
-  margin-top: 13rem;
+  margin-top: 10rem;
 }
 .font-bold {
   font-weight: 700;
---
+++ frontend/src/components/LogoBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- logo -->
+    <div class="logo">
+      <span class="font-bold text-xl">A<span class="signature-color">i</span>Letter</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogoBar",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo {
+  text-align: left;
+  margin: 2rem 0 0 2rem;
+}
+.signature-color {
+  color: rgb(146, 208, 80);
+}
+.font-bold {
+  font-weight: 700;
+}
+.text-xl {
+  font-size: 1.25rem;
+}
+</style>
