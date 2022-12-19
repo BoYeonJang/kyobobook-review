314c8a7e81efe08b47e447b4a071c5b155afa265
BoYeonJang <uko010199@gmail.com>
2022-12-20 00:55:57 +0900

    frontend: 백엔드 baseURL 주소 변경

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 // 로컬
 // const baseURL = "http://127.0.0.1:5000/";
 // 배포
-const baseURL = process.env.VUE_APP_BACKEND_URL;
+// const baseURL = process.env.VUE_APP_BACKEND_URL;
+const baseURL =
+  "https://port-0-kyobobook-review-python-flask-server-53px25lbux1h4i.gksl2.cloudtype.app/";
 const get = async endPoint => {
   console.log("request URL", baseURL + endPoint);
   return await fetch(baseURL + endPoint);
