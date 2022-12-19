8ac35af0c0229f0691c2bb5b31c6b8e3b15ed785
BoYeonJang <uko010199@gmail.com>
2022-12-20 02:02:43 +0900

    backend: import 삭제

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from generation_inference import *
 from classification_inference import *
 
